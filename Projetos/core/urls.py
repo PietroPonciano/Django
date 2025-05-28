93c83d32e58a3753c810abdfb9e91b60cbc45a99
PietroPonciano <pietro.ponciano2023@gmail.com>
2025-05-28 09:46:12 -0300

    Delete Projetos directory

--- Projetos/core/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import index, contato, produto
-from django.urls import path
-
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('contato', contato),
-    path('produto/<int:pk>', produto, name='produto')
-]
