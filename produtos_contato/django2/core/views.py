84590cfdee57d8aca4a6505f66545d46231293e1
PietroPonciano <pietro.ponciano2023@gmail.com>
2025-06-02 12:55:26 -0300

    Add files via upload

---
+++ produtos_contato/django2/core/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .forms import ContatoForm, ProdutoModelForm
+from django.contrib import messages
+def index(request):
+    return render(request, 'index.html')
+
+def contato(request):
+    form = ContatoForm (request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.send_mail()
+
+            messages.success(request, 'E-mail enviado com sucesso')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+def produto(request):
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+
+            form.save()
+            messages.success(request, 'Produto salvo com sucesso.')
+            form = ProdutoModelForm()
+        else:
+            messages.error(request, 'Erro ao salvar produto.')
+
+    else:
+        form = ProdutoModelForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'produto.html', {'form': form})
